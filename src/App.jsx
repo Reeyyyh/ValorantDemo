6a8962a1ae004188bdf23d920ffa6d178ac467e2
Reeyyyh <rhnlnv@gmail.com>
2025-01-25 21:05:55 +0700

    add footer in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Sidebar from "./Components/Sidebar";
 import AppRoutes from "./Routes/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -8,10 +9,15 @@ function App() {
       <div className="flex h-screen">
         {/* Sidebar */}
         <Sidebar />
-        
+
         {/* Main Content */}
-        <div className="flex-1 p-4 overflow-auto">
-          <AppRoutes />
+        <div className="flex-1 flex flex-col">
+          <div className="flex-1 p-4 overflow-auto">
+            <AppRoutes />
+          </div>
+
+          {/* Footer */}
+          <Footer />
         </div>
       </div>
     </Router>
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="bg-slate-900 text-center text-white py-4">
+            <h1 className="text-sm md:text-base font-semibold">
+                2025 Valorant Demo Website &copy; All Rights Reserved
+            </h1>
+        </footer>
+    );
+};
+
+export default Footer;
